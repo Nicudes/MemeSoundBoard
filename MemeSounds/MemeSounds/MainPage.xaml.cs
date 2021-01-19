a184bd5e1dfad22c542bb3ca84c0e85c77ea7229
Huyie <Huyie@DESKTOP-SOMD8VD>
2021-01-20 00:28:07 +0100

    Make It possible to play sound, added SImpleAudioPlayer as a nuget and implemented the function. Also made it shut down any sound when you tab down or close the program.

--- MemeSounds/MemeSounds/MainPage.xaml.cs
+++ MemeSounds/MemeSounds/MainPage.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Plugin.SimpleAudioPlayer;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -15,9 +18,55 @@ namespace MemeSounds
             InitializeComponent();
         }
 
+        protected override void OnDisappearing()
+        {
+            player.Stop();
+            base.OnDisappearing();
+        }
+
+        ISimpleAudioPlayer player = Plugin.SimpleAudioPlayer.CrossSimpleAudioPlayer.Current;
+
+
+        void PlaySound(string soundName)
+        {
+            player.Stop();
+            player.Load(GetStreamFromFile(soundName));
+            player.Play();
+        }
+
+        Stream GetStreamFromFile(string filename)
+        {
+            var assembly = typeof(App).GetTypeInfo().Assembly;
+
+            var stream = assembly.GetManifestResourceStream("MemeSounds.MemeSounds." + filename);
+
+            return stream;
+        }
+
         private void SearchBar_SearchButtonPressed(object sender, EventArgs e)
         {
 
         }
+
+        private void One_Clicked(object sender, EventArgs e)
+        {
+            PlaySound("Bruh.mp3");
+        }
+
+        private void Two_Clicked(object sender, EventArgs e)
+        {
+            PlaySound("OhNo.mp3");
+        }
+
+        private void Three_Clicked(object sender, EventArgs e)
+        {
+            PlaySound("Nani.mp3");
+        }
+
+        private void Four_Clicked(object sender, EventArgs e)
+        {
+            PlaySound("WinXP.mp3");
+        }
+
     }
 }
