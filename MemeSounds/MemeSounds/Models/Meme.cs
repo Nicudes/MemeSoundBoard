1969e985f2aa3ea3366f66e060dd927f545173d4
Huy Tran <Nicudes@gmail.com>
2021-01-22 15:56:21 +0100

    New model and added images for icons

---
+++ MemeSounds/MemeSounds/Models/Meme.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MemeSounds.Models
+{
+    public class Meme
+    {
+        public string MemeName { get; set; }
+        public string MemeCategory { get; set; }
+        public string SearchWords { get; set; } 
+        
+    }
+}
---
+++ MemeSounds/MemeSounds/Services/MemeData.cs
@@ -0,0 +1,33 @@
+﻿using MemeSounds.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MemeSounds.Services
+{
+    public class MemeData
+    {
+        public static List<Meme> MemeList = new List<Meme>();
+
+
+
+        public void GetMemeList()
+        {
+            MemeList.Add(new Meme() { 
+            
+                MemeName = "",
+                MemeCategory = "",
+                SearchWords = ""
+            });
+
+            MemeList.Add(new Meme()
+            {
+
+                MemeName = "",
+                MemeCategory = "",
+                SearchWords = ""
+            });
+        }
+
+    }
+}
