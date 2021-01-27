a04e62aa8f0b4664bb594ad91068daa25b808fbb
Huy Tran <Nicudes@gmail.com>
2021-01-27 15:43:19 +0100

    LogoFixed

--- MemeSounds/MemeSounds.Android/MainActivity.cs
+++ MemeSounds/MemeSounds.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace MemeSounds.Droid
 {
-    [Activity(Label = "MemeSounds", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "MeMe Soundz", Icon = "@drawable/MemeLogo4", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
